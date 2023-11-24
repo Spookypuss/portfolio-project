6c964383853a77794619cdb2fcb74acef41bb70a
Spookypuss <114418331+Spookypuss@users.noreply.github.com>
2023-11-24 14:10:04 +0000

    Implemented homepage design, added alert with contact details

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function contactMe() {
+    alert("email me at lynrycroft@gmail.com");
+}
+
+let contactLink = document.querySelector("#contact-me");
+contactLink.addEventListener("click", contactMe)
